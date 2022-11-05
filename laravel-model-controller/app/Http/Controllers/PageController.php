3720d4bae4a4f14518f2b2d21f8f2004cbdad46d
ErosEros <eros.vittori1994@gmail.com>
2022-11-05 18:43:02 +0100

    PageController get all Movie

--- laravel-model-controller/app/Http/Controllers/PageController.php
+++ laravel-model-controller/app/Http/Controllers/PageController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\models\Movie;
+
 
 class PageController extends Controller
 {
     //
 
     public function index(){
+        $movies = Movie::all();
+
         return 'ciao';
     }
 }
