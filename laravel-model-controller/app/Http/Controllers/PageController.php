9282c3482247d15b1aa1a055de79576d8fea4d9c
ErosEros <eros.vittori1994@gmail.com>
2022-11-05 18:47:28 +0100

    add view home

--- laravel-model-controller/app/Http/Controllers/PageController.php
+++ laravel-model-controller/app/Http/Controllers/PageController.php
@@ -13,6 +13,6 @@ class PageController extends Controller
     public function index(){
         $movies = Movie::all();
 
-        return 'ciao';
+        return view('home' , compact('movies'));
     }
 }
