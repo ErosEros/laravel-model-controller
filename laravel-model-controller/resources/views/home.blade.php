b9af781daa708df84e1838436c776982997bfec2
ErosEros <eros.vittori1994@gmail.com>
2022-11-07 00:41:43 +0100

    css

--- laravel-model-controller/resources/views/home.blade.php
+++ laravel-model-controller/resources/views/home.blade.php
@@ -1,6 +1,18 @@
-@foreach ($movies as $item)
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/app.css">
+    <title>laravel model</title>
+</head>
+<body>
+    @foreach ($movies as $item)
     <div>
         <h2>{{$item['title']}}</h2>
         <p>{{$item['vote']}}</p>
     </div>
 @endforeach
+</body>
+</html>
